517c44551d53cac7ffbec49f5ae7273e5967d35f
Rafaela <rafaelaolbo@gmail.com>
2026-03-25 11:40:12 -0300

    crud com node

---
+++ crud-node.js/controllers/todoController.js
@@ -0,0 +1,44 @@
+const Todo = require("../models/todo");
+
+exports.getAllTodos = (req, res) => {
+  Todo.getAllTodos((err, todos) => {
+    if (err) throw err;
+    res.json(todos);
+  });
+};
+
+exports.getTodoById = (req, res) => {
+  Todo.getTodoById(req.params.id, (err, todo) => {
+    if (err) throw err;
+    res.json(todo);
+  });
+};
+
+exports.createTodo = (req, res) => {
+  const newTodo = {
+    title: req.body.title,
+    completed: req.body.completed || false,
+  };
+  Todo.createTodo(newTodo, (err, result) => {
+    if (err) throw err;
+    res.json({ message: "Todo created successfully" });
+  });
+};
+
+exports.updateTodo = (req, res) => {
+  const updatedTodo = {
+    title: req.body.title,
+    completed: req.body.completed,
+  };
+  Todo.updateTodo(req.params.id, updatedTodo, (err, result) => {
+    if (err) throw err;
+    res.json({ message: "Todo updated successfully" });
+  });
+};
+
+exports.deleteTodo = (req, res) => {
+  Todo.deleteTodo(req.params.id, (err, result) => {
+    if (err) throw err;
+    res.json({ message: "Todo deleted successfully" });
+  });
+};
